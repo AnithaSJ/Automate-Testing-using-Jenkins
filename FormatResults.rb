06a5025fc567192b4ec44eb130249a86075d575e
Anitha S Jagadishwara <anithasj1789@gmail.com>
2018-10-11 14:18:31 +0200

    Excel file creation
    Format the results and store in excel file with customised file name

--- FormatResults.rb
+++ FormatResults.rb
@@ -47,6 +47,41 @@ class LogParser
 		outputFile.puts "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 		outputFile.close
+		#Excel file
+		workbook = RubyXL::Workbook.new
+		#Customised file name based on log file creation DateTime
+		log_creationTime = File.mtime("results.txt").to_s.split(" ")
+		cdate = log_creationTime[0]
+		ctime = log_creationTime[1].tr(':', '-')
+		filename = "Feedback_Test" + cdate + "_" + ctime +".xlsx"
+
+		##Important: Add/Modify cells in Sheets before .write method (creating excel file)
+		worksheet = workbook[0]
+		len = executedScripts_list.length
+
+		#First row in sheet >>Add heading
+		worksheet.add_cell(0, 0, "Test Scripts Filepath")
+		worksheet.add_cell(0, 1, "Test Results")
+		for i in 0..len
+		  #(row number|colum number| value)
+		  worksheet.add_cell(i+1, 0, executedScripts_list[i].to_s) #first column
+		#	puts executedScripts_list[i]
+		  worksheet.add_cell(i+1, 1, test_results[i]) #second column
+		end
+		col1_header_cell = worksheet[0][0]
+		col2_header_cell = worksheet[0][1]
+
+		col1_header_cell.change_font_bold(true)
+		col2_header_cell.change_font_bold(true)
+		#cell.fill_color
+		col1_header_cell.change_fill('ffff00')
+		col2_header_cell.change_fill('32cd32')
+		#creates Excel named as filename
+		workbook.write (filename)
+
+		## TODO: rename a worksheet first Sheet1
+		#workbook.worksheets[0].sheet_name = 'VW_VOC'
+		
 	end
 end
 
